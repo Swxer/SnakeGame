4d4df42fc8032502d5c3bc364666d47987db495b
Swxer <swxergamer64@gmail.com>
2026-02-12 18:07:28 +1100

    Created apple coord object with randomised x and y attributes

--- SnakeServer/Program.cs
+++ SnakeServer/Program.cs
@@ -2,6 +2,8 @@
 
 Coord gridDimensions = new Coord(50,20);
 Coord snakePos = new Coord(10,2);
+Random rand = new Random();
+Coord applePos = new Coord(rand.Next(1,gridDimensions.X-1),rand.Next(1,gridDimensions.Y-1));
 
 for (int y = 0; y < gridDimensions.Y; y++)
 {
@@ -10,6 +12,10 @@ for (int y = 0; y < gridDimensions.Y; y++)
         Coord currentCoord = new Coord(x, y);
         if (currentCoord.Equals(snakePos)) {
             Console.Write('■');   
+        }
+        else if (currentCoord.Equals(applePos))
+        {
+            Console.Write('O');
         }
         else if  (x == 0 || y == 0 || x == gridDimensions.X - 1 || y == gridDimensions.Y - 1)
         {

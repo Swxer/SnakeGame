d0ec71f3932580ff28697ba0e4e46a8c39087062
Swxer <swxergamer64@gmail.com>
2026-02-12 16:49:33 +1100

    Created Coord class

---
+++ SnakeServer/Coord.cs
@@ -0,0 +1,16 @@
+﻿
+namespace SnakeServer
+{
+    internal class Coord
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        
+        public Coord(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+    }
+}
+

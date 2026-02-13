316cf5be2cea7bc91cc5395c67c9de2611e83633
Swxer <swxergamer64@gmail.com>
2026-02-13 16:56:29 +1100

    Formatted files

--- SnakeServer/Coord.cs
+++ SnakeServer/Coord.cs
@@ -1,24 +1,23 @@
-﻿
-namespace SnakeServer
+﻿namespace SnakeServer
 {
     public class Coord : IEquatable<Coord>
     {
         public int X { get; private set; }
         public int Y { get; private set; }
-        
+
         public Coord(int x, int y)
         {
             this.X = x;
             this.Y = y;
         }
-        
+
         public bool Equals(Coord? other)
         {
             if (other is null) return false;
             return X == other.X && Y == other.Y;
         }
+
         public override bool Equals(object? obj) => Equals(obj as Coord);
         public override int GetHashCode() => HashCode.Combine(X, Y);
     }
 }
-
--- SnakeServer/Program.cs
+++ SnakeServer/Program.cs
@@ -1,9 +1,9 @@
 ﻿using SnakeServer;
 
-Coord gridDimensions = new Coord(50,20);
-Coord snakePos = new Coord(10,2);
+Coord gridDimensions = new Coord(50, 20);
+Coord snakePos = new Coord(10, 2);
 Random rand = new Random();
-Coord applePos = new Coord(rand.Next(1,gridDimensions.X-1),rand.Next(1,gridDimensions.Y-1));
+Coord applePos = new Coord(rand.Next(1, gridDimensions.X - 1), rand.Next(1, gridDimensions.Y - 1));
 int frameDelayMs = 100;
 
 while (true)
@@ -14,14 +14,15 @@ while (true)
         for (int x = 0; x < gridDimensions.X; x++)
         {
             Coord currentCoord = new Coord(x, y);
-            if (currentCoord.Equals(snakePos)) {
-                Console.Write('■');   
+            if (currentCoord.Equals(snakePos))
+            {
+                Console.Write('■');
             }
             else if (currentCoord.Equals(applePos))
             {
                 Console.Write('O');
             }
-            else if  (x == 0 || y == 0 || x == gridDimensions.X - 1 || y == gridDimensions.Y - 1)
+            else if (x == 0 || y == 0 || x == gridDimensions.X - 1 || y == gridDimensions.Y - 1)
             {
                 Console.Write('#');
             }
@@ -30,10 +31,9 @@ while (true)
                 Console.Write(' ');
             }
         }
+
         Console.WriteLine();
     }
+
     Thread.Sleep(frameDelayMs);
 }
-
-
-

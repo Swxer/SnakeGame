7a7826f99fdcd57b134e9628853faeb2be527c77
Swxer <swxergamer64@gmail.com>
2026-02-13 16:54:16 +1100

    Added frame delay for console clearing

--- SnakeServer/Program.cs
+++ SnakeServer/Program.cs
@@ -4,27 +4,36 @@ Coord gridDimensions = new Coord(50,20);
 Coord snakePos = new Coord(10,2);
 Random rand = new Random();
 Coord applePos = new Coord(rand.Next(1,gridDimensions.X-1),rand.Next(1,gridDimensions.Y-1));
+int frameDelayMs = 100;
 
-for (int y = 0; y < gridDimensions.Y; y++)
+while (true)
 {
-    for (int x = 0; x < gridDimensions.X; x++)
+    Console.Clear();
+    for (int y = 0; y < gridDimensions.Y; y++)
     {
-        Coord currentCoord = new Coord(x, y);
-        if (currentCoord.Equals(snakePos)) {
-            Console.Write('■');   
-        }
-        else if (currentCoord.Equals(applePos))
+        for (int x = 0; x < gridDimensions.X; x++)
         {
-            Console.Write('O');
+            Coord currentCoord = new Coord(x, y);
+            if (currentCoord.Equals(snakePos)) {
+                Console.Write('■');   
+            }
+            else if (currentCoord.Equals(applePos))
+            {
+                Console.Write('O');
+            }
+            else if  (x == 0 || y == 0 || x == gridDimensions.X - 1 || y == gridDimensions.Y - 1)
+            {
+                Console.Write('#');
+            }
+            else
+            {
+                Console.Write(' ');
+            }
         }
-        else if  (x == 0 || y == 0 || x == gridDimensions.X - 1 || y == gridDimensions.Y - 1)
-        {
-            Console.Write('#');
-        }
-        else
-        {
-            Console.Write(' ');
-        }
+        Console.WriteLine();
     }
-    Console.WriteLine();
+    Thread.Sleep(frameDelayMs);
 }
+
+
+

bf2ad7a7530e1812363050aa822f07c3c0cd10c8
Swxer <swxergamer64@gmail.com>
2026-02-21 22:10:30 +1100

    Implement snake tail functionality

--- SnakeServer/Program.cs
+++ SnakeServer/Program.cs
@@ -10,6 +10,8 @@ public class Program
         Rand.Next(1, GridDimensions.Y - 1));
 
     private static Direction _movementDirection = Direction.Down;
+    private static List<Coord> _snakeTail = new();
+    private static int _tailLength = 1;
 
     private static void Main(string[] args)
     {
@@ -20,8 +22,8 @@ public class Program
         {
             Console.SetCursorPosition(0, 0);
             _snakePos.ApplyMovementDirection(_movementDirection);
-
             RenderGame(GridDimensions, _snakePos, _applePos);
+            UpdateSnakeTail(_snakePos);
             GetMovementInput();
         }
     }
@@ -50,15 +52,22 @@ public class Program
         for (var y = 0; y < grid.Y; y++)
         {
             for (var x = 0; x < grid.X; x++)
-                if (x == snake.X && y == snake.Y)
+                if (x == snake.X && y == snake.Y || _snakeTail.Contains(new Coord(x, y)))
                     Console.Write('■');
                 else if (x == apple.X && y == apple.Y)
-                    Console.Write('O');
+                    Console.Write('A');
                 else if (x == 0 || y == 0 || x == grid.X - 1 || y == grid.Y - 1)
                     Console.Write('#');
                 else
                     Console.Write(' ');
             Console.WriteLine();
         }
+    }
+
+    private static void UpdateSnakeTail(Coord snake)
+    {
+        _snakeTail.Add(new Coord(snake.X, snake.Y));
+        if (_snakeTail.Count > _tailLength)
+            _snakeTail.RemoveAt(0);
     }
 }

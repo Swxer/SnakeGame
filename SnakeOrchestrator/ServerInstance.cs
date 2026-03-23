5b03fdaf07848a1df2120878ea570d48a84c04e0
Swxer <swxergamer64@gmail.com>
2026-03-24 00:01:16 +1100

    Create ServerInstance class

---
+++ SnakeOrchestrator/ServerInstance.cs
@@ -0,0 +1,9 @@
+﻿namespace SnakeOrchestrator;
+
+public class ServerInstance
+{
+    public int Port { get; set; }
+    public string ContainerId { get; set; } = "";
+    public int PlayerCount { get; set; }
+    public DateTime? EmptySince  { get; set; }
+}

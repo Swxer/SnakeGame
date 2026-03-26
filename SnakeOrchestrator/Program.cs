6648a78ebfe7ae431b0e6ea7b8d0fb0e6bc2f7ca
Swxer <swxergamer64@gmail.com>
2026-03-26 22:36:51 +1100

    Set up basic services in SnakeOrchestra/Program.cs

--- SnakeOrchestrator/Program.cs
+++ SnakeOrchestrator/Program.cs
@@ -1,7 +1,22 @@
-﻿public class Program
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using SnakeOrchestrator;
+
+public class Program
 {
+
     public static void Main(string[] args)
     {
-        Console.Write("This is Snake Orchestrator code block");
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddSingleton<OrchestratorService>();
+        builder.Services.AddSingleton<PollingService>();
+
+        var app = builder.Build();
+
+        var orchestrator = app.Services.GetRequiredService<OrchestratorService>();
+        var pollingService = app.Services.GetRequiredService<PollingService>();
     }
+
 }
+
